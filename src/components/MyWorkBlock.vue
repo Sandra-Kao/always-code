ef4b0ef7c8d7ced58cb5787d8207ba0997f74ab1
Sandra Kao <Sandra.Kao@advantech.com>
2021-07-20 20:36:13 +0800

    Add work cards

--- src/components/MyWorkBlock.vue
+++ src/components/MyWorkBlock.vue
@@ -1,11 +1,19 @@
 <template>
   <section class="my-work">
     <h2>My work</h2>
-    <div class="my-work__blobk">
-      <h3></h3>
+    <div
+      class="my-work__blobk"
+      v-for="type in workProjects"
+      :key="type.project"
+    >
+      <h3>{{ type.project }}</h3>
       <div class="my-work__wrapper">
-        <div class="my-work__card">
-          <a href=""> </a>
+        <div class="my-work__card" v-for="work in type.works" :key="work.name">
+          <a :href="work.link">
+            <h4>{{ work.name }}</h4>
+            <img :src="work.imgSrc" :alt="work.imgAlt" />
+            <p>{{ work.description }}</p>
+          </a>
         </div>
       </div>
     </div>
@@ -13,33 +21,81 @@
 </template>
 
 <script>
-export default { name: "MyWorkBlock",
-data(){
-  return{
-    workProjects:[
-      {
-        project:'Side projects',
-        works:[
-          {
-            url:'https://alwayscode.live/views/Index.html',
-            imgSrc:'',
-            imgAlt:'',
-            responsible:'I was responsible for',
-            description:'I was responsible for. I introduced and implemented training programs to the company.'
-          },
-          {
-            url:'https://todoitemsapiappservice.azurewebsites.net/api/TodoItems/{id}',
-            imgSrc:'',
-            imgAlt:'',
-            responsible:'I was responsible for',
-            description:'I was responsible for. I introduced and implemented training programs to the company.'
-          }
-        ]
-      }
-      ]
-    
-  }
-} };
+export default {
+  name: "MyWorkBlock",
+  data() {
+    return {
+      workProjects: [
+        {
+          project: "Side projects on my own",
+          works: [
+            {
+              name: "",
+              link: "https://alwayscode.live/views/Index.html",
+              imgSrc: "./assets/website-blog-post-vuejs.png",
+              imgAlt: "",
+              responsible: "I was responsible for",
+              description:
+                "I was responsible for. I introduced and implemented training programs to the company.",
+            },
+            {
+              name: "",
+              link: "https://alwayscode.live/views/Index.html",
+              imgSrc: "../assets/website-blog-post-vuejs.png",
+              imgAlt: "",
+              responsible: "I was responsible for",
+              description:
+                "I was responsible for. I introduced and implemented training programs to the company.",
+            },
+            {
+              name: "",
+              link: "https://alwayscode.live/views/Index.html",
+              imgSrc: "./assets/website-blog-post-vuejs.png",
+              imgAlt: "",
+              responsible: "I was responsible for",
+              description:
+                "I was responsible for. I introduced and implemented training programs to the company.",
+            },
+            
+          ],
+        },
+        {
+          project: "With Company",
+          works: [
+            {
+              name: "",
+              link: "https://alwayscode.live/views/Index.html",
+              imgSrc: "./assets/website-blog-post-vuejs.png",
+              imgAlt: "",
+              responsible: "I was responsible for",
+              description:
+                "I was responsible for. I introduced and implemented training programs to the company.",
+            },
+            {
+              name: "",
+              link: "https://alwayscode.live/views/Index.html",
+              imgSrc: "./assets/website-blog-post-vuejs.png",
+              imgAlt: "",
+              responsible: "I was responsible for",
+              description:
+                "I was responsible for. I introduced and implemented training programs to the company.",
+            },
+            {
+              name: "",
+              link: "https://alwayscode.live/views/Index.html",
+              imgSrc: "./assets/website-blog-post-vuejs.png",
+              imgAlt: "",
+              responsible: "I was responsible for",
+              description:
+                "I was responsible for. I introduced and implemented training programs to the company.",
+            },
+            
+          ],
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
